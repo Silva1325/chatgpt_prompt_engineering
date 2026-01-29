68b432b917dcb5cfe119b0c24c3c76d57d079bdb
Silva1325 <miguelsilva2001511@gmail.com>
2026-01-29 15:57:28 +0000

    transforming

---
+++ transforming.py
@@ -0,0 +1,85 @@
+import openai
+import os
+from dotenv import load_dotenv, find_dotenv
+from redlines import Redlines
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+    )
+    return response.choices[0].message["content"]
+
+# Translation
+
+prompt = f"""
+Translate the following English text to Spanish: \ 
+```Hi, I would like to order a blender```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Tell me which language this is: 
+```Combien coûte le lampadaire?```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Translate the following  text to French and Spanish
+and English pirate: \
+```I want to order a basketball```
+"""
+response = get_completion(prompt)
+print(response)
+
+# Format conversion
+
+data_json = { "resturant employees" :[ 
+    {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},
+    {"name":"Bob", "email":"bob32@gmail.com"},
+    {"name":"Jai", "email":"jai87@gmail.com"}
+]}
+
+prompt = f"""
+Translate the following python dictionary from JSON to an HTML \
+table with column headers and title: {data_json}
+"""
+response = get_completion(prompt)
+print(response)
+
+# Grammar check
+
+text = f"""
+Got this for my daughter for her birthday cuz she keeps taking \
+mine from my room.  Yes, adults also like pandas too.  She takes \
+it everywhere with her, and it's super soft and cute.  One of the \
+ears is a bit lower than the other, and I don't think that was \
+designed to be asymmetrical. It's a bit small for what I paid for it \
+though. I think there might be other options that are bigger for \
+the same price.  It arrived a day earlier than expected, so I got \
+to play with it myself before I gave it to my daughter.
+"""
+prompt = f"proofread and correct this review: ```{text}```"
+response = get_completion(prompt)
+print(response)
+
+
+diff = Redlines(text,response)
+#display(Markdown(diff.output_markdown))
+
+prompt = f"""
+proofread and correct this review. Make it more compelling. 
+Ensure it follows APA style guide and targets an advanced reader. 
+Output in markdown format.
+Text: ```{text}```
+"""
+response = get_completion(prompt)
+#display(Markdown(response))
